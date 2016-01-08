7ecdc238168157f4c5c9ad366bea41c37ff0b7e4
appendix <dszhengyu@qq.com>
2016-01-08 13:03:27 +0800

    add unix config backup

--- unix_config_backup/config_backup.sh
+++ unix_config_backup/config_backup.sh
@@ -4,4 +4,4 @@ cp ~/make_tag.sh ~/dszhengyu.github.io/unix_config_backup/
 cp ~/.vimrc ~/dszhengyu.github.io/unix_config_backup/
 cp ~/config_backup.sh ~/dszhengyu.github.io/unix_config_backup/
 
-cd ~/dszhengyu.github.io && git add . && git commit -m "add unix config backup"
+cd ~/dszhengyu.github.io && git add . && git commit -m "add unix config backup" && git push
