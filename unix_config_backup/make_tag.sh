39d582ddf7b82d4e565acd97545f3fa3cc331e06
appendix <dszhengyu@qq.com>
2016-01-29 12:58:37 +0800

    add unix config backup

--- unix_config_backup/make_tag.sh
+++ unix_config_backup/make_tag.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ctags -I __THROW -I "__nonnull ((1))" -I " __wur" --c++-kinds=+p --fields=+iaS --extra=+q -R /usr/include/ ~/program/ 
+ctags -I __THROW -I "__nonnull ((1))" -I " __wur" -I "_GLIBCXX_VISIBILITY(default)" -I "__gnu_cxx" \
+          --c++-kinds=+plx --fields=+iaS --extra=+q  --language-force=c++ \
+          -R /usr/include/ ~/program/ ~/cabinet/src/
